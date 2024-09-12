b1a046af0358dddfcaece8c4be180643c4559c52
jjmllr <68501961+jjmllr@users.noreply.github.com>
2024-09-12 17:28:44 +0200

    add plattform chip

---
+++ docs/src/components/PlatformChip.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+const PlatformChip = ({ platform  }) => (
+  // make a chip that displays the level of knowledge needed
+  <div style={{
+    display: "inline-block",
+    backgroundColor: "#2C60AD",
+    color: "#fff",
+    borderRadius: "25px",
+    paddingTop: "5px",
+    paddingBottom: "5px",
+    paddingLeft: "15px",
+    paddingRight: "15px",
+    fontSize: "14px",
+    fontWeight: "600",
+    margin: "10px",
+  }}>
+    Platform: {platform}
+  </div>
+
+);
+
+export default PlatformChip;
