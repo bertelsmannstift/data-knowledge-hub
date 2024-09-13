8a1643307feeca5d9bfdae34cd6345dd75ad551b
jjmllr <68501961+jjmllr@users.noreply.github.com>
2024-09-13 14:37:25 +0200

    Add Last Updated by and picture clara

--- docs/src/components/LastUpdatedByChip.js
+++ docs/src/components/LastUpdatedByChip.js
@@ -2,22 +2,22 @@ import React from "react";
 
 const LastUpdatedByChip = ({ author, updatedOn  }) => (
   // make a chip that displays the level of knowledge needed
-  <div style={{
-    display: "inline-block",
-    color: "#2C60AD",
-    borderRadius: "25px",
-    paddingTop: "5px",
-    paddingBottom: "5px",
-    paddingLeft: "0px",
-    paddingRight: "15px",
-    fontSize: "14px",
-    fontWeight: "600",
-    margin: "0px",
-    marginBottom: "10px",
-  }}>
-    last updated by {author} on {updatedOn}
-  </div>
-
+  <div style={{ width: "100%" }}>
+    <div style={{
+      display: "inline-block",
+      textAlign: "right",
+      color: "#2C60AD",
+      paddingTop: "0px",
+      paddingBottom: "0px",
+      paddingLeft: "0px",
+      fontSize: "14px",
+      fontWeight: "600",
+      margin: "0px",
+      width: "100%", // Ensure the inner div takes up full width
+    }}>
+      last updated by {author} on {updatedOn}
+    </div>
+</div>
 );
 
 export default LastUpdatedByChip;
