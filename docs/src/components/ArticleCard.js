0d9305a7b9e06d188bd94e0b849a34a4f0df3289
jjmllr <68501961+jjmllr@users.noreply.github.com>
2025-01-21 11:13:52 +0100

    Add h1 background

--- docs/src/components/ArticleCard.js
+++ docs/src/components/ArticleCard.js
@@ -8,19 +8,19 @@ const ArticleCard = ({ heading, author, organization, teaser, link }) => (
       color: "#000",
       backgroundColor: "#fff",
       borderRadius: "5px",
-      padding: "15px",
+      padding: "40px",
       marginBottom: "10px",
-      border: "1px solid #e0e0e0",
-      boxShadow: "0 2px 5px rgba(0, 0, 0, 0.1)",
+      border: "2px solid #73F19C",
+      boxShadow: "0 2px 5px #73F19C",
     }}
   >
     {/* Heading */}
-    <div style={{ fontWeight: "bold", fontSize: "18px", marginBottom: "5px" }}>
+    <div style={{ fontWeight: "bold", fontSize: "18px", marginBottom: "0px", paddingBottom:"0px" }}>
       {heading}
     </div>
 
     {/* Author and Organization */}
-    <div style={{ fontSize: "14px", color: "#555", marginBottom: "10px" }}>
+    <div style={{ fontSize: "14px", color: "black", marginBottom: "10px" }}>
       <span style={{ fontStyle: "italic" }}>
         by {author} | {organization}
       </span>
@@ -39,12 +39,12 @@ const ArticleCard = ({ heading, author, organization, teaser, link }) => (
         rel="noopener noreferrer"
         style={{
           fontSize: "14px",
-          color: "#2C60AD",
+          color: "black",
           fontWeight: "bold",
           textDecoration: "none",
         }}
       >
-        Read more
+        Read more <img src="/img/icons/chevron-right.svg" alt="arrow right" style={{marginLeft: '4px'}} />
       </a>
     )}
   </div>
